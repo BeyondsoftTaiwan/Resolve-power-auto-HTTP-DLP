16fe0fa7d07b5cb4aebed090f709d848367483aa
JessicaNa0808 <v-jessna@microsoft.com>
2026-08-20 11:10:22 +0800

    Update function_app.py

--- function_app.py
+++ function_app.py
@@ -1,19 +1,15 @@
 import azure.functions as func
 import json
- 
 app = func.FunctionApp()
- 
 @app.route(
 route="GetRepositories",
 auth_level=func.AuthLevel.FUNCTION
 )
 def GetRepositories(req: func.HttpRequest) -> func.HttpResponse:
- 
 result = {
 "success": True,
 "message": "PowerAutoDLP is running"
 }
- 
 return func.HttpResponse(
 json.dumps(result),
 mimetype="application/json",
