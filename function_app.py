8baea01955a867a8f7c4c48b15cadbaa89ff13d2
JessicaNa0808 <v-jessna@microsoft.com>
2026-08-24 15:01:43 +0800

    Refactor GetPullRequests to handle pagination

--- function_app.py
+++ function_app.py
@@ -58,15 +58,22 @@ def GetRepositories(req: func.HttpRequest) -> func.HttpResponse:
     auth_level=func.AuthLevel.FUNCTION
 )
 def GetPullRequests(req: func.HttpRequest) -> func.HttpResponse:
-    logger.info("GetPullRequests received: method=%s", req.method)
+    logger.info(
+        "GetPullRequests received: method=%s", 
+        req.method
+    )
+    
     try:
         managed_identity_client_id = "f9de4e70-47fc-490a-8d58-4f65840d4e16"
+        
         logger.info("Creating Azure credential chain")
+        
         credential = DefaultAzureCredential(
             managed_identity_client_id=managed_identity_client_id
         )
         logger.info("Azure credential chain created")
         logger.info("Requesting Azure DevOps access token")
+        
         token = credential.get_token(
             "499b84ac-1321-427f-aa17-267ca6975798/.default"
         )
@@ -79,61 +86,82 @@ def GetPullRequests(req: func.HttpRequest) -> func.HttpResponse:
             "https://dev.azure.com/"
             "microsoft/"
             "e5547036-015b-4291-9a77-28151a645368/"
-            "_apis/git/repositories/"\
+            "_apis/git/repositories/"
             "00c2b511-7cf2-462b-8b31-1cc0ab0a7cf3/"
             "pullRequests"
         )
-        params = {
-            "searchCriteria.status": "all",
-            "$top": 100,
-            "api-version": "7.1"
-        }
+
+        all_pull_requests = []
+        skip = 0
+        page_size = 100
         
-        logger.info("Calling Azure DevOps Pull Request API")
-        pr_response = requests.get(
-            url,
-            headers=headers,
-            params=params,
-            timeout=60
-        )
-        logger.info(
-            "Azure DevOps response status=%s",
-            pr_response.status_code
-        )
-        if pr_response.status_code != 200:
-            return func.HttpResponse(
-                pr_response.text,
-                mimetype="application/json",
-                status_code=pr_response.status_code
+        while True:
+            params = {
+                "searchCriteria.status": "all",
+                "$top": page_size,
+                "&skip": skip,
+                "api-version": "7.1"
+            }
+            logger.info("Calling Azure DevOps Pull Request API")
+            pr_response = requests.get(
+                url,
+                headers=headers,
+                params=params,
+                timeout=60
             )
-        data = pr_response.json()
-        pull_requests = []
-        for pr in data.get("value", []):
-            pull_requests.append({
-                "pullRequestId": pr.get("pullRequestId"),
-                "title": pr.get("title"),
-                "status": pr.get("status"),
-                "creationDate": pr.get("creationDate"),
-                "createdBy": (
-                    pr.get("createdBy", {})
-                    .get("displayName")
-                ),
-                "repositoryName": (
-                    pr.get("repository", {})
-                    .get("name")
-                ),
-                "sourceBranch": pr.get("sourceRefName"),
-                "targetBranch": pr.get("targetRefName"),
-                "url": pr.get("url")
-            })
+            logger.info(
+                "Azure DevOps response status=%s",
+                pr_response.status_code
+            )
+            if pr_response.status_code != 200:
+                return func.HttpResponse(
+                    pr_response.text,
+                    mimetype="application/json",
+                    status_code=pr_response.status_code
+                )
+                
+            data = pr_response.json()
+            current_page = data.get("value",[])
+            if not current_page:
+                break
+            for pr in current_page:
+                all_pull_requests.append({
+                    "pullRequestId": pr.get("pullRequestId"),
+                    "title": pr.get("title"),
+                    "status": pr.get("status"),
+                    "creationDate": pr.get("creationDate"),
+                    "closedDate": pr.get("closedDate"),
+                    "createdBy": (
+                        pr.get("createdBy", {})
+                        .get("displayName")
+                    ),
+                    "repositoryName": (
+                        pr.get("repository", {})
+                        .get("name")
+                    ),
+                    "sourceBranch": pr.get(
+                        "sourceRefName"
+                    ),
+                    "targetBranch": pr.get(
+                        "targetRefName"
+                    ),
+                    "mergeStatus": pr.get(
+                        "mergeStatus"
+                    ),
+                    "url": pr.get("url")
+                })
+            if len(current_page) < page_size:
+                break
+            skip += page_size
+            
             result = {
                 "status": "success",
-                "count": len(pull_requests),
-                "pullRequests": pull_requests
+                "count": len(all_pull_requests),
+                "pullRequests": all_pull_requests
             }
             logger.info(
                 "GetPullRequests completed: count=%s",
-                len(pull_requests)
+                len(all_pull_requests)
                 )
             return func.HttpResponse(
                 json.dumps(result),
